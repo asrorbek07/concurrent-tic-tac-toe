23d357a5916da9117eea3e2d68f16ccf6b40618a
asrorbek07 <asrorbekazamjon@gmail.com>
2023-12-23 10:22:20 +0500

    done

--- src/main/java/com/epam/rd/autocode/concurrenttictactoe/PlayerImpl.java
+++ src/main/java/com/epam/rd/autocode/concurrenttictactoe/PlayerImpl.java
@@ -1,6 +1,8 @@
 package com.epam.rd.autocode.concurrenttictactoe;
 
 
+import java.util.Map;
+
 public class PlayerImpl implements Player{
     private TicTacToe ticTacToe;
     char mark;
@@ -14,28 +16,33 @@ public class PlayerImpl implements Player{
 
     @Override
     public void run() {
-        try {
-            while (checkIfGotChance(ticTacToe.table())) {
-            synchronized (ticTacToe) {
-                    if (ticTacToe.lastMark() == mark) {
-                        // wait for other player to make a move
-                        ticTacToe.wait();
-                    }
-                    else   {
-                        // other player has already made a move, so exit
-                        Move move = strategy.computeMove(mark, ticTacToe);
-                        ticTacToe.setMark(move.row, move.column, mark);
-                        ticTacToe.notifyAll();
-                    }
+        while (true) {
+        synchronized (ticTacToe) {
+            if (!checkIfGotChance(ticTacToe.table())){
+                break;
+            }
+                if (ticTacToe.lastMark() == mark) {
+                    // wait for other player to make a move
+//
+                }
+                else   {
+                    // other player has already made a move, so exit
+                    Move move = strategy.computeMove(mark, ticTacToe);
+                    ticTacToe.setMark(move.row, move.column, mark);
+
+//                        ticTacToe.notifyAll();
                 }
             }
-            Thread.currentThread().interrupt();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        }
+        Map<Thread, StackTraceElement[]> threads = Thread.getAllStackTraces();
+        for (Thread thread : threads.keySet()) {
+            if (thread.getName().contains("Thread")) {
+                thread.interrupt();
+            }
         }
     }
 
-    private boolean checkIfGotChance(char[][] table) {
+    private synchronized boolean checkIfGotChance(char[][] table) {
         return !(table[0][2] == table[1][1] && table[1][1] == table[2][0] && table[1][1] != ' '||
                 table[0][0] == table[1][1] && table[1][1] == table[2][2] && table[1][1] != ' ' ||
                 table[0][0] == table[0][1] && table[0][1] == table[0][2] && table[0][1] != ' ' ||
